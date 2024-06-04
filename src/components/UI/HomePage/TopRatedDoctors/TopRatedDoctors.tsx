c657a163a077ccffc52b1b59386b6d2ac41b5ed8
Koushik Ahmad <koushikahmad123@gmail.com>
2024-06-04 11:12:41 +0600

    added module 44.9

---
+++ src/components/UI/HomePage/TopRatedDoctors/TopRatedDoctors.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from "@mui/material";
+
+const TopRatedDoctors = () => {
+  return (
+    <Box
+      sx={{
+        my: 10,
+        py: 30,
+        backgroundColor: "rgba(20, 20, 20, 0.1)",
+        clipPath: "polygon(0 0, 100% 25%, 100% 100%, 0 75%)",
+      }}
+    >
+      <Box sx={{ textAlign: "center" }}>
+        <Typography variant="h4" component="h1" fontWeight={700}>
+          Our Top Rated Doctors
+        </Typography>
+        <Typography component="p" fontSize={18} fontWeight={400} sx={{ mt: 2 }}>
+          Access to expert physicians and surgeons, advanced technologies
+        </Typography>
+        <Typography component="p" fontSize={18} fontWeight={400}>
+          and top-quality surgery facilities right here.
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default TopRatedDoctors;
